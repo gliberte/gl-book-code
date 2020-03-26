1885b1810926d6acf58969ba4abe2400a20026db
Luis Solano <luis.solano.l@gmail.com>
2020-03-26 18:10:21 -0500

    ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import mapboxgl from 'mapbox-gl'
+import styled from 'styled-components'
+import cases_country from './data/cases_country'
 
+const MapContainer = styled.div`
+  width:100%;
+  height:100vh;
+`
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 function App() {
+  useEffect(()=>{
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v9'
+    })
+    map.on('load',()=>{
+      map.addSource('covid-cases-source',{
+        type:"geojson",
+        data:cases_country
+      })
+      map.addLayer({
+        id:"covid-cases-layer",
+        type:'circle',
+        source:'covid-cases',
+        paint:{
+          'circle-color':'red'
+        }
+      })
+    })
+  })
   return (
-    <div>
-      <h1>Mapbox App</h1>
-    </div>
+    <MapContainer id="map">
+      
+    </MapContainer>
   );
 }
 
